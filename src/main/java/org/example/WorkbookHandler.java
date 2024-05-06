f3374b38be82c7adf624c305093b379afb0f88d3
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-05-06 20:15:54 +0400

    feat: Add new method to WorkbookHandler class

--- src/main/java/org/example/WorkbookHandler.java
+++ src/main/java/org/example/WorkbookHandler.java
@@ -11,6 +11,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.example.PageManager.loadCurrentPage;
@@ -46,7 +48,7 @@ public class WorkbookHandler {
             sheet = wb.createSheet("Autos");
             createHeaderRow(sheet);
         }
-        addData(sheet);
+        addData();
         try (OutputStream os = new FileOutputStream(filename)) {
             wb.write(os);
         } catch (IOException e) {
@@ -71,7 +73,7 @@ public class WorkbookHandler {
         header.createCell(13).setCellValue("Situation");
         header.createCell(14).setCellValue("Market");
     }
-    private static void addData(Sheet sheet) throws IOException {
+    private static void addData() throws IOException {
 
         Properties prop = new Properties();
         try {
@@ -84,82 +86,44 @@ public class WorkbookHandler {
         String baseUrl = prop.getProperty(BASE_URL);
         int currentPage = loadCurrentPage();
 
+        List<Auto> autoAds = new ArrayList<>();
         for (int i = currentPage; i < currentPage + 5; i++) {
             System.out.println("Page "+ i + " is parsing");
             String url = baseUrl + i;
             Document doc = Jsoup.connect(url + i).get();
             Elements adLinks = doc.select(".tz-container .products .products-i__link");
-            for (Element adLink : adLinks) {
 
+            for (Element adLink : adLinks) {
                 String adUrl = adLink.attr("abs:href");
                 Document doc1 = Jsoup.connect(adUrl).get();
-
-                String price = doc1.select(".product-price__i.product-price__i--bold").text();
-
-                Elements propertiesColumns = doc1.select(".product-properties__column");
-
-                Row newRow = sheet.createRow(sheet.getLastRowNum() + 1);
-                newRow.createCell(0).setCellValue(price);
-                for (Element propertiesColumn : propertiesColumns) {
-
-                    Elements elements = propertiesColumn.select(".product-properties__i ");
-                    for (Element element : elements) {
-
-                        Element elementLabel = element.select("label").first();
-                        String attr = elementLabel.attr("for");
-                        String elementSpan = element.select("span").first().text();
-
-                        switch (attr){
-                            case "ad_region":
-                                newRow.createCell(1).setCellValue(elementSpan);
-                                break;
-                            case "ad_make_id":
-                                newRow.createCell(2).setCellValue(elementSpan);
-                                break;
-                            case "ad_model":
-                                newRow.createCell(3).setCellValue(elementSpan);
-                                break;
-                            case "ad_reg_year":
-                                newRow.createCell(4).setCellValue(elementSpan);
-                                break;
-                            case "ad_category":
-                                newRow.createCell(5).setCellValue(elementSpan);
-                                break;
-                            case "ad_color":
-                                newRow.createCell(6).setCellValue(elementSpan);
-                                break;
-                            case "ad_engine_volume":
-                                newRow.createCell(7).setCellValue(elementSpan);
-                                break;
-                            case "ad_mileage":
-                                newRow.createCell(8).setCellValue(elementSpan);
-                                break;
-                            case "ad_transmission":
-                                newRow.createCell(9).setCellValue(elementSpan);
-                                break;
-                            case "ad_gear":
-                                newRow.createCell(10).setCellValue(elementSpan);
-                                break;
-                            case "ad_new":
-                                newRow.createCell(11).setCellValue(elementSpan);
-                                break;
-                            case "ad_seats_count":
-                                newRow.createCell(12).setCellValue(elementSpan);
-                                break;
-                            case "ad_Vəziyyəti":
-                                newRow.createCell(13).setCellValue(elementSpan);
-                                break;
-                            case "ad_market":
-                                newRow.createCell(14).setCellValue(elementSpan);
-                                break;
-                            default:
-                        }
-                    }
-                }
+                Auto auto = parseAutoAd(doc1);
+                autoAds.add(auto);
             }
         }
         currentPage += 5;
         saveCurrentPageNumber(currentPage);
         System.out.println("Current Page " + currentPage);
     }
+    private static Auto parseAutoAd(Document document) {
+
+        String price = document.select(".product-price__i.product-price__i--bold").text();
+        String region = document.select("[for=ad_region] span").text();
+        String brand = document.select("[for=ad_make_id] span").text();
+        String model = document.select("[for=ad_model] span").text();
+        String graduationYear = document.select("[for=ad_reg_year] span").text();
+        String banType = document.select("[for=ad_category] span").text();
+        String color = document.select("[for=ad_color] span").text();
+        String engine = document.select("[for=ad_engine_volume] span").text();
+        String mileage = document.select("[for=ad_mileage] span").text();
+        String transmission = document.select("[for=ad_transmission] span").text();
+        String gear = document.select("[for=ad_gear] span").text();
+        String isNew = document.select("[for=ad_new] span").text();
+        String seats = document.select("[for=ad_seats_count] span").text();
+        String situation = document.select("[for=ad_Vəziyyəti] span").text();
+        String market = document.select("[for=ad_market] span").text();
+
+        return new Auto(price, region, brand, model, graduationYear, banType, color, engine, mileage,
+                transmission, gear, isNew, seats, situation, market);
+    }
+
 }
